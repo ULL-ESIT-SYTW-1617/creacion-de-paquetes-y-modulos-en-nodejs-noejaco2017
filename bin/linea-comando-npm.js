4231ec01b79ffe95d0edf35992c606c50769250c
alu0100622492 <alu0100622492@ull.edu.es>
2016-10-06 23:44:46 +0000

    Creado package.json a traves de file system

--- bin/linea-comando-npm.js
+++ bin/linea-comando-npm.js
@@ -1,23 +1,84 @@
+var express = require('express')
+var app = express()
+var path = require('path');
 var fs= require('fs');
 var ejs=require('ejs');
+
+//DIRECTORIO CON TEMPLATES
+app.set('views', path.join(__dirname, 'template'));
+//app.set('views', path.join(__dirname, '../assets/*'));
+app.set('view engine', 'ejs');
+
+//MINIMIST
 var argv = require('minimist')(process.argv.slice(2));
-//console.dir(argv);
+console.dir(argv);
+
+//EJS RENDERFILE cargamos la plantilla
+ejs.renderFile('./template/package.ejs',function(err, result) {
+   // render on success
+   if (!err) {
+    
+            console.log(result);
+            //CREAMOS EL PACKAGE:JSON en template
+               var write=fs.writeFile("./template/package.json",result, (err) => {
+                       if (err) throw err;
+                       console.log('It\'s saved!');
+             });
+   }
+   // render or error
+   else {
+            console.log('An error occurred: renderFile(package.ejs)');
+            console.log(err);
+   }
+});
+
+console.log("argv_",argv._);
+console.log("argv_'",argv._[0]);
+console.log("argv_1",argv._[1]);
+console.log("argv",argv);
+console.log("argv1"+argv[1]);
+console.log("argv2"+argv[2]);
+
+
 
 //console.log("argv_: "+argv._);//contiene los argumentos no asociados al --
 //console.log("opts"+argv.opts.string);
-fs.readFile("./template/package.ejs",function(error,data){
-    //console.log("contiene el fichero: "+ data.length());
-    // console.log(data);
-    //console.log("dataname"+data.version);
-});
+// console.log("argv"+argv);
+// console.log("argv._" ,argv._);
+// console.log ("argv._0",argv[0]);
+// console.log("argv._1"+argv[1]);
+// console.log("argv._2"+argv[2]);
 
-var content= fs.readFileSync("./template/package.ejs");//en la variable content tenemos guardado el package.ejs
+ 
+
+
+//  var read=fs.readFile("./template/package.ejs",function(error,data){
+//     //console.log("contiene el fichero: "+ data.length());
+//      //console.log("dato"+data);
+//      console.log("dataname_ "+data[0].name);
+//     //console.log("dataname"+data.version);
+// });
+
+// console.log("READ : "+read);
+// var content= fs.readFileSync("./template/package.ejs");//en la variable content tenemos guardado el package.ejs
+// //console.log("content"+content);
+// var configuracion = JSON.parse(content);
+// console.log("configuracion_name : ",configuracion.name);
+
+// // ejs.fileLoader = function("./template/package.ejs") {
+// //   return 'new content + from file: ' + fs.readFileSync("./template/package.ejs");
+// // };
+
+// var p = require('ejs').renderFile("./template/package.ejs",{nombre:argv[0]});
+
 // console.log("content"+ content);
 // console.log("content"+ content.name);
 // console.log("ejs"+ejs.readFile("./template/package.ejs"));
 
 
-var configuracion = JSON.parse(content);
+
+
+
 
 // var autor = "<%= title.name =%>" = argv[1];
 //console.log("nombre: "+ configuracion.name);
@@ -31,7 +92,7 @@ var configuracion = JSON.parse(content);
 //var result = ejs.renderFile("./template/package.ejs", {name: argv._[0]}); 
 
 
-console.log("config: ", configuracion);
+//-----------------------------console.log("config: ", configuracion);
 
 // config.title.name = argv[0];
 // config.url.git = argv[1];
